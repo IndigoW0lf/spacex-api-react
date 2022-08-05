7628ba78162c2a395fcabd4b26daf8f9d01f485c
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2022-08-05 01:31:45 -0500

    Query company name and summary for homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+// components
+import Error from './../../Error';
+import Loader from './../../Loader';
+import MainHeader from './../../components/MainHeader';
 // style
 import './style.scss';
 
+const GET_COMPANY_INFO = gql`
+    {
+        company {
+            name
+            summary
+        }
+    }
+`
+
 const Home = () => {
-    return <div className="home__container d-flex align-items-center text-center"></div>;
+    const { data, loading, error } = useQuery(GET_COMPANY_INFO);
+
+    if (loading) return <Loader />
+    if (error) return <Error error={error} />
+
+    return (
+    <div className="home__container d-flex align-items-center text-center">
+        <MainHeader name={data.company.name} description={data.company.summary}/>
+    </div>
+    );
 };
 
 export default Home;
